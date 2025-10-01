90451ee6987fcf6d591a4c2f7faac500754cb87a
akhtarrahmani <168260752+AkhtarRehmani@users.noreply.github.com>
2025-10-01 16:43:46 +0500

    set live

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Site Config
-define('ENVIRONMENT', 'local'); // local | live
+define('ENVIRONMENT', 'live'); // local | live
 define('SITE_NAME', 'Little Black Limo');
 define('SITE_URL', 'http://localhost:8000'); // Local URL
 define('LIVE_URL', 'https://develop.a1rides.com.au'); // Live URL
