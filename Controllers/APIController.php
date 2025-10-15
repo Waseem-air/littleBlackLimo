c09641bf223adb3e1ed6ba5c66fd9bb999f98b6a
akhtarrahmani <168260752+AkhtarRehmani@users.noreply.github.com>
2025-10-15 13:05:00 +0500

    set perth timezone

--- Controllers/APIController.php
+++ Controllers/APIController.php
@@ -1,57 +1,4 @@
 <?php
-//function curlPost($postData,$endpoint){
-//    // Enable error reporting
-//    error_reporting(E_ALL);
-//    ini_set('display_errors', 1);
-//    $apiBaseUrl = $GLOBALS['API_URL'];
-//    $APItoken = API_TOKEN;
-//    $apiUrl = $apiBaseUrl.$endpoint;
-//    $ch = curl_init();
-//    curl_setopt_array($ch, [
-//        CURLOPT_URL => $apiUrl,
-//        CURLOPT_RETURNTRANSFER => true,
-//        CURLOPT_POST => true,
-//        CURLOPT_POSTFIELDS => http_build_query($postData),
-//        CURLOPT_HTTPHEADER => [
-//            'Accept: application/json',
-//            'Content-Type: application/x-www-form-urlencoded',
-//            'Authorization: Bearer ' . $APItoken
-//        ],
-//        CURLOPT_TIMEOUT => 30,
-//        CURLOPT_SSL_VERIFYPEER => false, // Set to true in production
-//    ]);
-//    $response = curl_exec($ch);
-//    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-//    $curlError = curl_error($ch);
-//    curl_close($ch);
-//    // Handle API response
-//    if ($curlError) {
-//        http_response_code(500);
-//        return [
-//            'success' => false,
-//            'message' => 'cURL Error: ' . $curlError
-//        ];
-//    }
-//
-//    $apiResponse = json_decode($response, true);
-//    if ($httpCode === 200 && isset($apiResponse['success']) && $apiResponse['success']) {
-//        return [
-//            'success' => true,
-//            'message' => 'Booking processed successfully',
-//            'data' => $apiResponse['data'],
-//        ];
-//    } else {
-//        http_response_code($httpCode);
-//        return [
-//            'success' => false,
-//            'message' => $apiResponse['message'] ?? 'API request failed',
-//            'api_errors' => $apiResponse['errors'] ?? [],
-//            'http_code' => $httpCode,
-//            'api_response' => $apiResponse
-//        ];
-//    }
-//}
-
 
 function curlPost($postData, $endpoint)
 {
@@ -62,7 +9,6 @@ function curlPost($postData, $endpoint)
     $apiBaseUrl = $GLOBALS['API_URL'];
     $APItoken = API_TOKEN;
     $apiUrl = $apiBaseUrl . $endpoint;
-
     $ch = curl_init();
     curl_setopt_array($ch, [
         CURLOPT_URL => $apiUrl,
