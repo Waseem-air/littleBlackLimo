894ee95f7bbe15449d6533bfff7b4c06e09a48a4
Muhammad Hassan <muhammadhassan5075729@gmail.com>
2025-10-22 04:42:37 -0700

    uplod live key

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 // ==============================
 // Base Config
 // ==============================
-define('ENVIRONMENT', 'local'); // local | live
+define('ENVIRONMENT', 'live'); // local | live
 define('SITE_NAME', 'Little Black Limo');
 define('SITE_URL', 'http://127.0.0.1:8000'); // Local URL
 define('LIVE_URL', 'https://a1rides.com.au'); // Live URL
@@ -16,8 +16,8 @@ $API_URL = (ENVIRONMENT === 'local') ? LOCAL_API_URL : LIVE_API_URL;
 // ==============================
 // API Key / Tokens
 // ==============================
-//define('API_TOKEN','nZRapXpITUnir8FrRuPNzyYrz8by9TIlOt5re9taic46rOB0DK6mMTBtV1Jp');   //live
-define('API_TOKEN', 'dlB4zJalzIsKiA3o4drTeKz93p7zkry9gEh9A98dNJdbqoCzpun8KV45hqhl');   //local
+define('API_TOKEN','nZRapXpITUnir8FrRuPNzyYrz8by9TIlOt5re9taic46rOB0DK6mMTBtV1Jp');   //live
+//define('API_TOKEN', 'dlB4zJalzIsKiA3o4drTeKz93p7zkry9gEh9A98dNJdbqoCzpun8KV45hqhl');   //local
 // ==============================
 // Helper Function for API Request
 // ==============================
